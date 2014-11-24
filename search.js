61b935c5d60fcc0f955d74fdc52353321a906762
Tom VanAntwerp <vanantwerp@gmail.com>
2014-11-24 15:20:44 -0500

    Initial commit
    Returns JSON of video ID and date published given a channel ID.

---
+++ search.js
@@ -0,0 +1,73 @@
+// Your use of the YouTube API must comply with the Terms of Service:
+// https://developers.google.com/youtube/terms
+
+// Helper function to display JavaScript value on HTML page.
+var results = [];
+var numResults = 0;
+
+// Called automatically when JavaScript client library is loaded.
+function onClientLoad() {
+    gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
+}
+
+// Called automatically when YouTube API interface is loaded (see line 9).
+function onYouTubeApiLoad() {
+    // This API key is intended for use only in this lesson.
+    // See http://goo.gl/PdPA1 to get a key for your own applications.
+    gapi.client.setApiKey('AIzaSyCR5In4DZaTP6IEZQ0r1JceuvluJRzQNLE');
+
+    firstSearch();
+}
+
+function firstSearch() {
+    // Use the JavaScript client library to create a search.list() API call.
+    var request = gapi.client.youtube.search.list({
+        part: 'id,snippet',
+        channelId: 'UCFJNcE0iHj7P6dhp5iCZRLg', //API key from codecademy
+        type: 'video'
+    });
+
+    // Send the request to the API server,
+    // and invoke onSearchRepsonse() with the response.
+    request.execute(onSearchResponse);
+}
+
+function nextSearch(nextPageToken) {
+    var request = gapi.client.youtube.search.list({
+        part: 'id,snippet',
+        channelId: 'UCFJNcE0iHj7P6dhp5iCZRLg',
+        pageToken: nextPageToken,
+        type: 'video'
+    });
+
+    request.execute(onSearchResponse);
+}
+
+// Called automatically with the response of the YouTube API request.
+function onSearchResponse(response) {
+    numResults += response.items.length;
+    if (response.nextPageToken) {
+        saveResult(response);
+        nextSearch(response.nextPageToken);
+    } else if (!response.nextPageToken) {
+        saveResult(response);
+        showResponse(results);
+    }
+}
+
+function showResponse(response) {
+    var responseString = JSON.stringify(response, '', 2);
+    document.getElementById('response').innerHTML += responseString;
+}
+
+function saveResult(response) {
+    for (var i = 0, l = response.items.length; i < l; i++) {
+
+        results.push({
+            id: response.items[i].id.videoId,
+            date: response.items[i].snippet.publishedAt,
+
+        });
+        document.getElementById('count').innerHTML = numResults;
+    }
+}
